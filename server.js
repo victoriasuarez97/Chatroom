a7896e7a0c9609398db2f5806481f75ef3ad9d8b
Victoria Suarez <victoriasuarez97@gmail.com>
2020-07-19 22:50:39 -0300

    landing page and chatroom done + new dependencies

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = 3000 || process.env.PORT;
+io.on('connection', socket => {
+  console.log(`New WebSocket connection`);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
