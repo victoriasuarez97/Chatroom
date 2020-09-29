b5d38bdea6cdfa4b3ab569b94262aa1f711b5662
Victoria Suarez <victoriasuarez97@gmail.com>
2020-09-28 21:45:52 -0300

    bootstrap cdns deleted and created new assets folder with boostrap scripts

--- server.js
+++ server.js
@@ -65,5 +65,5 @@ io.on('connection', socket => {
   });
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
